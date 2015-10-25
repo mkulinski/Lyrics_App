4f2f2c1696dbe48b46e5689f16a4238efdda932d
Michael Kulinski <me@michaelkulinski.com>
2015-10-25 15:37:18 -0500

    heading, genres and featured done

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name purelyricsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the purelyricsApp
+ */
+angular.module('purelyricsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
